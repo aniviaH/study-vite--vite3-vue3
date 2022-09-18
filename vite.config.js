ce159e9219a7226aa68d808f3476edb2ca2daab8
1058039938@qq.com <aniviah@163.com>
2022-09-18 18:10:25 +0800

    feat: vite.config--resolve.alias, css

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path, { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      'comps': path.resolve(__dirname, 'src/components')
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `$injectedColor: red`
+      },
+      sass: {
+        additionalData: `$injectedColor: yellow`
+      },
+      styl: {
+        additionalData: `$injectedColor ?= orange`
+      }
+    }
+  },
   plugins: [vue()]
 })
